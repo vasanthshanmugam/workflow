b8ce4ce3c220e3d8cc7e47068afefa66f3f751b5
vasanthshanmugam <32473491+vasanthshanmugam@users.noreply.github.com>
2024-06-01 19:50:47 +0100

    test

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+  name = "nginx:latest"
+}
+
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "example-nginx"
+  ports {
+    internal = 80
+    external = 8080
+  }
+}
